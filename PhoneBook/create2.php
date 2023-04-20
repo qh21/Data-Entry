dadf2c8ffa6c5a48f417028325d0254050eed212
Qassem Al-Hanbali <76710890+qh21@users.noreply.github.com>
2023-04-20 14:15:35 +0300

    Update create2.php

--- PhoneBook/create2.php
+++ PhoneBook/create2.php
@@ -11,12 +11,31 @@ $con = new mysqli($serverName, $dbuserName, $dbpassword, $dbName);
     $Username = $_POST['Username'];
     $Password = $_POST['Password'];
 
+$sql1 = "SELECT * from users where Username = '$Username'";
+$res1 = $con->query($sql1);
+if ($res1->num_rows == 1){
 
-     
-$sql1 = "INSERT INTO users (ID, uName, Username, Password)
+
+    echo "<title>Account Already Exists</title>";
+    echo "<style>body{background-color:#395a79;color:#001a33;font-size:x-large;text-align:center;} fieldset{width:50%;margin-left:25%}";
+    echo ".link,.link:visited{text-decoration:none;  color: darkblue;} .link:hover{color: #003c8b;}</style>";
+    echo "<h1 align='center'>Account Already Exists!<br>Try Another Username</h1>";
+echo "<style> .table3{width:25%;margin-left:40%;font-size:x-large;}</style>";
+echo "<br><table class='table3' align='center'><tr><td><a href='create.php' class='link'>Back</a></td>";
+echo "<td><td><a href='index.php' class='link'>Login Screen</a></td></tr></table>";
+
+
+
+}
+
+
+else
+{
+
+$sql2 = "INSERT INTO users (ID, uName, Username, Password)
  VALUES (DEFAULT, '$uName', '$Username', '$Password');";
 
-$res1 = $con->query($sql1);
+$res2 = $con->query($sql2);
 session_start();
 
 $_SESSION["userName"] = $Username;
@@ -31,7 +50,7 @@ echo "<form method='post' id='homepageform' action='main.php'><br><table class='
 echo "<td><input name='userName' value='".$Username."' hidden>";
 echo "<input name='pwd' value='".$Password."' hidden><a class='link' onclick='submitFunction()'>Home Page</a></td></tr></table></form>";
 echo "<script>function submitFunction() {document.getElementById('homepageform').submit();}</script>";
-
+}
 $con->close();
 
 
